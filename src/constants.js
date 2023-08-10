d221f263253b806ea2547fa20d4fa91a55b0d8d5
SaiAbhiRam <saiabhiram1928@gamil.com>
2023-08-10 12:45:58 +0530

    build production -1

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = 'http://localhost:5000'; // If using proxy
+export const BASE_URL = 'https://shoppy2-backend-saiabhiram.onrender.com'; // If using proxy
 export const PRODUCTS_URL = '/products';
 export const USERS_URL = '/auth';
 export const ORDERS_URL = '/orders';
